0db981dfa46f2cd1c45e44e0aad419b7ce611302
alvinC <lvinz0790@gmail.com>
2021-01-12 21:43:15 -0800

    longest palindrome

---
+++ leetcode/03.longest_palindrome.js
@@ -0,0 +1,24 @@
+// Given a string s, return the longest palindromic substring in s.
+
+var longestPalindrome = function(s) {
+    let current = "";
+    let longest = "";
+    debugger
+    for(let i = 0; i < s.length; i++) {
+        for(let j = i; j < s.length; j++) {
+            let sub = s.slice(i, j + 1);
+            let reverseSub = sub.split("").reverse().join("");
+            if (sub === reverseSub) {
+                debugger
+                current = sub
+            }
+            if (current.length > longest.length) {
+                longest = current
+            }
+        }
+    }
+    return longest;
+};
+
+// let veryLong = "anugnxshgonmqydttcvmtsoaprxnhpmpovdolbidqiyqubirkvhwppcdyeouvgedccipsvnobrccbndzjdbgxkzdbcjsjjovnhpnbkurxqfupiprpbiwqdnwaqvjbqoaqzkqgdxkfczdkznqxvupdmnyiidqpnbvgjraszbvvztpapxmomnghfaywkzlrupvjpcvascgvstqmvuveiiixjmdofdwyvhgkydrnfuojhzulhobyhtsxmcovwmamjwljioevhafdlpjpmqstguqhrhvsdvinphejfbdvrvabthpyyphyqharjvzriosrdnwmaxtgriivdqlmugtagvsoylqfwhjpmjxcysfujdvcqovxabjdbvyvembfpahvyoybdhweikcgnzrdqlzusgoobysfmlzifwjzlazuepimhbgkrfimmemhayxeqxynewcnynmgyjcwrpqnayvxoebgyjusppfpsfeonfwnbsdonucaipoafavmlrrlplnnbsaghbawooabsjndqnvruuwvllpvvhuepmqtprgktnwxmflmmbifbbsfthbeafseqrgwnwjxkkcqgbucwusjdipxuekanzwimuizqynaxrvicyzjhulqjshtsqswehnozehmbsdmacciflcgsrlyhjukpvosptmsjfteoimtewkrivdllqiotvtrubgkfcacvgqzxjmhmmqlikrtfrurltgtcreafcgisjpvasiwmhcofqkcteudgjoqqmtucnwcocsoiqtfuoazxdayricnmwcg";
+let veryShort = "lol"
