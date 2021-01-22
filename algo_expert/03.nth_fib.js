eae0d4e65be91f6258021cf7f4da5e18ffd46d7e
alvinC <lvinz0790@gmail.com>
2021-01-21 21:36:07 -0800

    nth fibonacci

--- algo_expert/03.nth_fib.js
+++ algo_expert/03.nth_fib.js
@@ -1,3 +1,4 @@
+// recursive 
 function getNthFib(n, memo={1: 0, 2: 1}) {
 	if(n in memo) {
 		return memo[n];
@@ -6,6 +7,26 @@ function getNthFib(n, memo={1: 0, 2: 1}) {
 		return memo[n];
 	}
 }
-
 // time : o(n) because of memo, if not it will be o(n^2)
 // space : o(n) callstacks stores linear space
+
+
+
+// iterative
+function getNthFib(n) {
+    let currentFib = [0, 1];
+    let count = 3;
+
+    while(count <= n) {
+        let nextFib = currentFib[0] + currentFib[1];
+        currentFib.shift();
+        currentFib.push(nextFib);
+        count++;
+    }
+    if(n === 1) return 0;
+    return currentFib[1];
+}
+
+// time : o(n);
+// space: o(1);
+
