b4bc16e4726c4549ed0c553ba4ad27650dbe5552
alvinC <lvinz0790@gmail.com>
2021-01-27 16:43:11 -0800

    depth first search

---
+++ algo_expert/06.depth_first_search.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+		array.push(this.name);
+		this.children.forEach((child) => {
+			child.depthFirstSearch(array);
+		})
+		return array;
+	}
+}
+// time o(v + e) vertex(node) + edge
+// space o(v) by the number of vertex
+
+// {
+//   "graph": {
+//     "nodes": [
+//       {"children": ["B", "C", "D"], "id": "A", "value": "A"},
+//       {"children": ["E", "F"], "id": "B", "value": "B"},
+//       {"children": [], "id": "C", "value": "C"},
+//       {"children": ["G", "H"], "id": "D", "value": "D"},
+//       {"children": [], "id": "E", "value": "E"},
+//       {"children": ["I", "J"], "id": "F", "value": "F"},
+//       {"children": ["K"], "id": "G", "value": "G"},
+//       {"children": [], "id": "H", "value": "H"},
+//       {"children": [], "id": "I", "value": "I"},
+//       {"children": [], "id": "J", "value": "J"},
+//       {"children": [], "id": "K", "value": "K"}
+//     ],
+//     "startNode": "A"
+//   }
+// }
