5e61569305c701f7244e86d62330afedd3146cfb
alvinC <lvinz0790@gmail.com>
2021-02-03 13:45:51 -0800

    merge intervals

--- leetcode/43.merge_intervals.js
+++ leetcode/43.merge_intervals.js
@@ -1,3 +1,15 @@
+// 56. Merge Intervals
+
+// Given an array of intervals where intervals[i] = [starti, endi], merge all 
+// overlapping intervals, and return an array of the non-overlapping intervals 
+// that cover all the intervals in the input.
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
 var merge = function(intervals) {
    intervals.sort((a, b) => a[0] - b[0])
     let result = [];
@@ -21,3 +33,7 @@ var merge = function(intervals) {
     return result;
 
 };
+
+// time o(n log(n)) because of the sort.
+// space o(n) because of result array
+// https://leetcode.com/problems/merge-intervals/
