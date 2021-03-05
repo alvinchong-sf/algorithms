6024a0ef3849c5460894fc3e2f619531ce02956a
alvinC <lvinz0790@gmail.com>
2021-03-05 03:22:32 -0800

    reverse linked list

---
+++ leetcode/64.reverse_ll.js
@@ -0,0 +1,44 @@
+// 206. Reverse Linked List
+
+// Given the head of a singly linked list, reverse the list, and return 
+// the reversed list.
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+var reverseList = function(head) {
+    if(!head) return null;
+    let arr = [];
+    let currentNode = head;
+    while(currentNode) {
+        arr.push(currentNode);
+        currentNode = currentNode.next;
+    }
+    arr[0].next = null;
+    for(let i = 1; i < arr.length; i++) {
+        let prevNode = arr[i - 1];
+        let currNode = arr[i];
+        currNode.next = prevNode;
+    }
+    return arr[arr.length - 1];
+};
+
+// time o(n)
+// space o(n)
+////////////////////////////////////////////////////////////////////////////////////
+
+var reverseList = function(head) { 
+    let prevNode = null;                 
+    let currentNode = head;               
+    while(currentNode) {
+        let nextNode = currentNode.next;
+        currentNode.next = prevNode;
+        prevNode = currentNode;    
+        currentNode = nextNode;
+    }
+    return prevNode;
+};
+
+// time o(n)
+// space o(1) extra
+// https://leetcode.com/problems/reverse-linked-list/
