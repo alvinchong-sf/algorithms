f2107b4a527cf7c1e5c9e91fd7e78432cb4a58ce
alvinC <lvinz0790@gmail.com>
2021-03-29 23:06:13 -0700

    find successor

--- algo_expert/49.find_successor.js
+++ algo_expert/49.find_successor.js
@@ -32,3 +32,55 @@ const inOrder = (node, array) => {
 
 // time o(2n) => o(n)
 // space o(2n) => o(n)
+
+///////////////////////////////////////////////////////////////////////////////
+
+// brute force logarithmic time complexity 
+
+function findSuccessor(tree, node) {
+	// inorder => left -> value -> right
+	
+	// if no parent,no left and no right 
+	if(!node.parent && !node.right && !node.left) return null; // o(1)
+	
+	// if no left or right, and is left child, we return parent
+	if(!node.left && !node.right && node.parent.left === node) return node.parent; // o(1)
+	
+	// if no left or right, and is right child, we ask if our parent have a parent. if yes we return that grand parent and if not return null
+	if(!node.left && !node.right && node.parent.right === node) { // o(1)
+		if(node.parent.parent === null) {
+			return null
+		} else {
+			return node.parent.parent
+		}
+	}
+	
+	// if no right, and we are left child, return parent
+	if(!node.right && node.parent.left === node) return node.parent; // o(1)
+	
+	// if no right, and we are right child, return null
+	if(!node.right && node.parent.right === node) return null;  // o(1)
+	
+	// if we have left and right, we go right and find the most left child
+	if(node.left && node.right) { // o(log(n))
+		currentNode = node.right;
+		while(currentNode.left) {
+			currentNode = currentNode.left
+		}
+		return currentNode
+	}
+	
+	// if is root and no right
+	if(!node.parent && !node.right) return null // o(1)
+	
+	// if root of tree
+	if(!node.parent) return node.right // o(1)
+	
+}
+
+// time o(log(n))
+// space o(1)
+
+///////////////////////////////////////////////////////////////////////////////
+
+
