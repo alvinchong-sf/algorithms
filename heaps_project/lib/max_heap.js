f98cf6c438f1666f19e2fa7e1943eb36baab2744
alvinC <lvinz0790@gmail.com>
2021-04-04 01:09:45 -0700

    max heap

--- heaps_project/lib/max_heap.js
+++ heaps_project/lib/max_heap.js
@@ -4,60 +4,73 @@ class MaxHeap {
     }
 
     getParent(idx) {
-        return Math.floor( idx / 2 );
+        return Math.floor(idx / 2);
     }
 
     getLeftChild(idx) {
+        // left child are even
         return idx * 2;
     }
 
     getRightChild(idx) {
-        return idx * 2 + 1
+        // right child are odd
+        return (idx * 2) + 1;
     }
 
     siftUp(idx) {
-        if (idx === 1) return;
+        // if array was originally empty; we can return immediately
+        if(idx === 1) return;
+
         let parentIdx = this.getParent(idx);
 
-        if(this.array[idx] > this.array[parentIdx]) {
-            [ this.array[parentIdx], this.array[idx] ] = [ this.array[idx], this.array[parentIdx] ];
-            this.siftUp(parentIdx);
+        // if child is larger than parent, keep swapping
+        if(this.array[parentIdx] < this.array[idx]) {
+            [this.array[idx], this.array[parentIdx]] = [this.array[parentIdx], this.array[idx]]
         }
+        // recursively call the parent
+        this.siftUp(parentIdx);
     }
 
     insert(val) {
+        // [null, 40, 20, 21, 7]
         this.array.push(val);
         this.siftUp(this.array.length - 1);
     }
 
     siftDown(idx) {
-        let ary = this.array;
         let leftIdx = this.getLeftChild(idx);
         let rightIdx = this.getRightChild(idx);
-        let leftVal = ary[leftIdx];
-        let rightVal = ary[rightIdx];
+        let leftVal = this.array[leftIdx] !== undefined ? this.array[leftIdx] : -Infinity;
+        let rightVal = this.array[rightIdx] !== undefined ? this.array[rightIdx] : -Infinity;
 
-        if(leftVal === undefined) leftVal = -Infinity;
-        if(rightVal === undefined) rightVal = -Infinity;
+        if(this.array[idx] > leftVal && this.array[idx] > rightVal) return;
 
-        if(ary[idx] > leftVal && ary[idx] > rightVal) return;
-
-        if(leftVal < rightVal) {
-            var swapIdx = rightIdx;
+        let swapIdx;
+        if(leftVal >= rightVal) {
+            swapIdx = leftIdx;
         } else {
-            var swapIdx = leftIdx;
+            swapIdx = rightIdx;
         }
-        [ ary[idx], ary[swapIdx] ] = [ ary[swapIdx], ary[idx] ];
 
+        [this.array[idx], this.array[swapIdx]] = [this.array[swapIdx], this.array[idx]];
         this.siftDown(swapIdx);
     }
 
     deleteMax() {
-        if(this.array.length === 2) return this.array.pop();
-        if(this.array.length === 1) return null;
-
+        // only a single element in the heap array
+        if(this.array.length === 2) {
+            return this.array.pop();
+        }
+        // if nothing but the null in the heap array
+        if(this.array.length <= 1) {
+            return null;
+        }
+        
+        // get reference to the ele we gona delete
         let max = this.array[1];
-        this.array[1] = this.array.pop();
+        // take the last item from the heap and set it as the first ele
+        let last = this.array.pop();
+        this.array[1] = last;
 
         this.siftDown(1);
         return max;
