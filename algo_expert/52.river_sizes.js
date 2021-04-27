d4e1f3e29d5336a78f334340c86548c9e14dade4
alvinC <lvinz0790@gmail.com>
2021-04-26 21:50:56 -0700

    river sizes refactor

--- algo_expert/52.river_sizes.js
+++ algo_expert/52.river_sizes.js
@@ -1,46 +1,58 @@
 function riverSizes(matrix) {
-  let resultArr = [];
-	const visitedMatrix = matrix.map(row => row.map(value => false));
+  // resultArr = [2,1,5,2,2]
+	// count = 2
+	// 2 nested loops; i and j
+		// [V, 0, 0, V, 0],
+    // [V, 0, V, 0, 0],
+    // [0, 0, V, 0, V],
+    // [V, 0, V, 0, V],
+    // [V, 0, V, V, 0]
+	// if current iteration == 0 || == "V"; skip or continue
+	// if current iteration == 1; dfs / bfs
+	// dfs / bfs needs to consider out of bounds
+	// left j >= 0 / right j <= matrix[0].length - 1 / top i >= 0 / bottom i <= matrix.length - 1
+	// if visited mark as "V" and increment count++
+	// at the end of bfs / dfs => resultArr.push(count)
+	// return resultArr at the end
+	
+	let resultArr = [];
 	for(let i = 0; i < matrix.length; i++) {
-		for(let j = 0; j < matrix[i].length; j++) {
-			if(visitedMatrix[i][j] === true) continue;
-			dfs(i, j, matrix, visitedMatrix, resultArr);
+		for(let j = 0; j < matrix[0].length; j++) {
+			if(matrix[i][j] === 0 || matrix[i][j] === "V") {
+				continue;
+			} else if (matrix[i][j] === 1) {
+				let count = dfs(i, j, matrix);
+				resultArr.push(count);
+			}
 		}
 	}
 	return resultArr;
 }
 
-const dfs = (i, j, matrix, visitedMatrix, resultArr) => {
-	let count = 0;
+const dfs = (i, j, matrix) => {
+	let counter = 0;
 	let stack = [[i, j]];
 	
 	while(stack.length) {
-		let currentNode = stack.pop();
-		i = currentNode[0];
-		j = currentNode[1];
-		if(visitedMatrix[i][j] === true) continue;
-		visitedMatrix[i][j] = true;
-		if(matrix[i][j] === 0) continue;
-		count++;
-		const unvisitedNeighbors = getUnvisitedNeighbors(i, j, matrix, visitedMatrix);
-		for(const neighbor of unvisitedNeighbors) {
-			stack.push(neighbor)
+		let river = stack.pop();
+		let idx1 = river[0];
+		let idx2 = river[1];
+		
+		if(matrix[idx1][idx2] === 1) {
+			matrix[idx1][idx2] = "V";
+			counter++;
+			
+			// left
+			if(idx2 - 1 >= 0) stack.push([idx1, idx2 - 1]);
+			// right
+			if(idx2 + 1 <= matrix[0].length - 1) stack.push([idx1, idx2 + 1])
+			// top
+			if(idx1 - 1 >= 0) stack.push([idx1 - 1, idx2]);
+			// down
+			if(idx1 + 1 <= matrix.length - 1) stack.push([idx1 + 1, idx2])
 		}
 	}
-	if(count > 0) resultArr.push(count);
-}
-
-const getUnvisitedNeighbors = (i, j, matrix, visitedMatrix) => {
-	const unvisitedArr = [];
-	// top
-	if(i > 0 && !visitedMatrix[i - 1][j]) unvisitedArr.push([i - 1, j]);
-	// bottom
-	if(i < matrix.length - 1 && !visitedMatrix[i + 1][j]) unvisitedArr.push([i + 1, j]);
-	// left 
-	if(j > 0 && !visitedMatrix[i][j - 1]) unvisitedArr.push([i, j - 1]);
-	// right 
-	if(j < matrix[0].length - 1 && !visitedMatrix[i][j + 1]) unvisitedArr.push([i, j + 1])
-	return unvisitedArr;
+	return counter;
 }
 
 // time o(n * m)
