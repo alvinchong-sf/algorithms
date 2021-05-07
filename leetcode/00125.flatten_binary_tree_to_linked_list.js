52276e36237b92ef83a81e594f0403af1c45f4ec
alvinC <lvinz0790@gmail.com>
2021-05-07 16:24:54 -0700

    flatten binary tree to linked list o(h) space solution

---
+++ leetcode/00125.flatten_binary_tree_to_linked_list.js
@@ -0,0 +1,34 @@
+// 114. Flatten Binary Tree to Linked List
+
+// Given the root of a binary tree, flatten the tree into a "linked list":
+
+// The "linked list" should use the same TreeNode class where the right child 
+// pointer points to the next node in the list and the left child pointer is always null.
+// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+
+var flatten = function(root) {
+    if(!root) return;
+    if(!root.left && !root.right) return;
+    
+    let stack = [root]; // stack = [5,4]
+    // 1-> 2 ->3->4
+    while(stack.length) {
+        let node = stack.pop(); // node = 3
+        if(node.right) stack.push(node.right);
+        if(node.left) stack.push(node.left);
+        
+        if(!stack.length) {
+            node.right = null
+        } else {
+            node.right = stack[stack.length - 1];   
+        }
+        node.left = null;
+    }
+    return;
+    
+};
+
+// time o(n)
+// space o(h)
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
