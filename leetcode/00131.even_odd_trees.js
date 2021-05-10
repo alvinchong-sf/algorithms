c796e69691e8a05697e601a49ec6c38fa2f79471
alvinC <lvinz0790@gmail.com>
2021-05-10 14:42:18 -0700

    even odd trees

--- leetcode/00131.even_odd_trees.js
+++ leetcode/00131.even_odd_trees.js
@@ -1,27 +1,30 @@
 var isEvenOddTree = function(root) {
-    let queue = [root];
-    let idx = 0;
-    
-    while(queue.length){
-        let size = queue.length;
-        let prev = 0;
-        
-        for(let i = 0; i < size; i++){
-            let node = queue.shift();
-            if(idx % 2 === 0){
+    let level = 0;   
+    let queue = [root];            
+    //                                  
+    while(queue.length) {
+        let size = queue.length;    
+        let prevVal = (level % 2 === 0) ? -Infinity : Infinity 
+        for(let i = 0; i < size; i++) { 
+            let node = queue.shift();  
+            // even level
+            if(level % 2 === 0) {       
+                // if node is not odd and is not increasing
                 if(node.val % 2 === 0) return false;
-                if(prev !== 0 && prev >= node.val) return false;
-            }else{
-                if(node.val % 2 === 1) return false;
-                if(prev !== 0 && prev <= node.val) return false;
+                if(prevVal >= node.val) return false;
+            } // odd level 
+            else {
+                // if node is not even and is not decreasing
+                if(node.val % 2 !== 0) return false;
+                if(node.val >= prevVal) return false;
             }
+            
             if(node.left) queue.push(node.left);
             if(node.right) queue.push(node.right);
-            prev = node.val;
+            prevVal = node.val
         }
-        idx++;
+        level++;
     }
-    
     return true;
 };
 
