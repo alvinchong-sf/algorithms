bd9ee39d378a3bfffa29e7c239f9a2cc77813525
alvinC <lvinz0790@gmail.com>
2021-05-11 17:53:37 -0700

    binary tree diameter

--- leetcode/00138.binary_tree_diameter.js
+++ leetcode/00138.binary_tree_diameter.js
@@ -13,7 +13,27 @@ const findHeight = (root) => {
     let right = findHeight(root.right);
     return 1 + Math.max(left, right);
 }
-
-// time o(n^ 2)
+// time o(n^2)
 // space o(n)
 // https://leetcode.com/problems/diameter-of-binary-tree/
+
+
+
+var diameterOfBinaryTree = function(root) {
+    let diameter = 0;  
+    
+    const dfs = (root) => {
+        if(!root) return 0;
+        let left = dfs(root.left); 
+        let right = dfs(root.right); 
+        let sum = left + right; 
+        diameter = Math.max(diameter, sum); 
+        return 1 + Math.max(left, right);
+        
+    }
+    dfs(root);
+    return diameter;
+};
+
+// time o(n)
+// space o(h) if balanced; if not o(n);
