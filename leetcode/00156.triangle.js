031c979890ff5ac79ee667492041361647ea727d
alvinC <lvinz0790@gmail.com>
2021-05-19 00:09:18 -0700

    triangle

--- leetcode/00156.triangle.js
+++ leetcode/00156.triangle.js
@@ -1,3 +1,19 @@
+// 120. Triangle
+// Given a triangle array, return the minimum path sum from top to bottom.
+
+// For each step, you may move to an adjacent number of the row below. More 
+// formally, if you are on index i on the current row, you may move to either 
+// index i or index i + 1 on the next row.
+
+// Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+// Output: 11
+// Explanation: The triangle looks like:
+//    2
+//   3 4
+//  6 5 7
+// 4 1 8 3
+// The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+
 
 // DFS
 var minimumTotal = function(triangle) {
@@ -15,7 +31,7 @@ var minimumTotal = function(triangle) {
         dfs(triangle, i + 1, j + 1, currSum);
     }
     
-    dfs(triangle, 0 , 0, 0);
+    dfs(triangle, 0, 0, 0);
     
     return minSum;
 };
