2f49ad54afe457dd1ed251f687c80ba7446af199
alvinC <lvinz0790@gmail.com>
2021-06-04 18:16:15 -0700

    word search

---
+++ leetcode/00165.word_search.js
@@ -0,0 +1,38 @@
+var exist = function(board, word) {
+    
+    for(let row = 0; row < board.length; row++) {
+        for(let col = 0; col < board[0].length; col++) {
+            if(board[row][col] === word[0]) {
+                let bool = dfs(board, row, col, word, 0)
+                if(bool) return true;
+            }
+        }
+    }
+    return false;
+};
+
+const dfs = (board, row, col, word, i) => {
+    if(row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
+        return false;
+    }
+    if(board[row][col] !== word[i]) return false;
+    
+    if(i === word.length - 1) return true;
+    let temp = board[row][col];
+    board[row][col] = true;
+    
+    // top
+    let top = dfs(board, row - 1, col, word, i + 1);
+    
+    // right
+    let right = dfs(board, row, col + 1, word, i + 1);
+    
+    // left 
+    let left = dfs(board, row, col - 1, word, i + 1);
+    
+    // bottom
+    let bottom = dfs(board, row + 1, col, word, i + 1);
+    
+    board[row][col] = temp;
+    return top || right || left || bottom;
+}
