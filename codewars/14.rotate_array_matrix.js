285c45b7c643bc6ee28a9783a717ab5319346612
alvinC <lvinz0790@gmail.com>
2021-06-18 22:17:09 -0700

    rotate clockwise and counter clockwise matrix

---
+++ codewars/14.rotate_array_matrix.js
@@ -0,0 +1,62 @@
+// Write a rotate function that rotates a two-dimensional array (a matrix) either 
+// clockwise or anti-clockwise by 90 degrees, and returns the rotated array.
+// The function accepts two parameters: an array, and a string specifying the 
+// direction or rotation. The direction will be either "clockwise" or "counter-clockwise".
+// Here is an example of how your function will be used:
+
+// var matrix = [[1, 2, 3],
+//               [4, 5, 6],
+//               [7, 8, 9]];
+
+// rotate(matrix, "clockwise"); // Would return  [[7, 4, 1], [8, 5, 2],  [9, 6, 3]]
+// To help you visualize the rotated matrix, here it is formatted as a grid:
+
+//  [[7, 4, 1],
+//   [8, 5, 2],
+//   [9, 6, 3]]
+// Rotated counter-clockwise it would looks like this:
+
+//  [[3, 6, 9],
+//   [2, 5, 8],
+//   [1, 4, 7]]
+
+function rotate(matrix, direction) {
+  if(direction === "clockwise") {
+    return clockwiseRotate(matrix)
+  } else {
+    return counterClockwiseRotate(matrix)
+  }
+}
+
+// clockwise 
+// tranpose and reverse
+const clockwiseRotate = (matrix) => {
+  const outerArr = [];
+  for(let col = 0; col < matrix[0].length; col++) {
+    const innerArr = [];
+    for(let row = 0; row < matrix.length; row++) {
+      innerArr.push(matrix[row][col]);
+    }
+    const reverseArr = innerArr.reverse();
+    outerArr.push(reverseArr);
+  }
+  return outerArr;
+};
+
+
+// counter-clockwise
+// tranpose and unshift 
+const counterClockwiseRotate = (matrix) => {
+  const outerArr = [];
+  for(let col = 0; col < matrix[0].length; col++) {
+    const innerArr = [];
+    for(let row = 0; row < matrix.length; row++) {
+      innerArr.push(matrix[row][col]);
+    }
+    outerArr.unshift(innerArr);
+  }
+  return outerArr;
+};
+
+// time o(n^2) | space o(n^2)
+// https://www.codewars.com/kata/525a566985a9a47bc8000670/train/javascript
