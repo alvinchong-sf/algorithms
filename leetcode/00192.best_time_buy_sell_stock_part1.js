1c528b5f776fdc4f8f44edba6385eaddad028a42
alvinC <lvinz0790@gmail.com>
2021-06-22 13:20:24 -0700

    best time to buy and sell stock

--- leetcode/00192.best_time_buy_sell_stock_part1.js
+++ leetcode/00192.best_time_buy_sell_stock_part1.js
@@ -14,6 +14,7 @@
 // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
+// greedy
 var maxProfit = function(prices) {
     let maxProfit = 0;
     let minBuy = prices[0];
@@ -24,6 +25,24 @@ var maxProfit = function(prices) {
     }
     return maxProfit;
 };
-
 // time o(n) | space o(1)
+
+// DP
+var maxProfit = function(prices) {
+    if(prices.length < 2) return 0;
+    const table = new Array(prices.length).fill(-Infinity);
+    
+    for(let i = 0; i < prices.length - 1; i++) {
+        const buy = prices[i];
+        for(let j = i + 1; j < prices.length; j++) {
+            const sell = prices[j];
+            const profit = sell - buy;
+            table[j] = Math.max(table[j], profit)
+        }
+    }
+    const res = Math.max(...table);
+    return res < 0 ? 0 : res;
+};
+// time o(n^2) | space o(n)
+
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/ 
