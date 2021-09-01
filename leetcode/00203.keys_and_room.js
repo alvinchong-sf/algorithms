913f6a901251d1af4ad47b155f0de9393c4f0267
alvinC <lvinz0790@gmail.com>
2021-08-31 18:42:35 -0700

    keys and room

--- leetcode/00203.keys_and_room.js
+++ leetcode/00203.keys_and_room.js
@@ -1,3 +1,5 @@
+
+// dfs
 var canVisitAllRooms = function(rooms) {
     const set = new Set([0]);
     
@@ -18,3 +20,21 @@ var canVisitAllRooms = function(rooms) {
 // time o(n); where n is the total elements in the 2d array when flatten
 // space o(n)
 // https://leetcode.com/problems/keys-and-rooms/
+
+
+// bfs 
+var canVisitAllRooms = function(rooms) {
+    const keys = new Set([0]);
+    const queue = [rooms[0]];
+    
+    while (queue.length) {
+        const room = queue.shift();
+        for (let i = 0; i < room.length; i++) {
+            const key = room[i];
+            if (keys.has(key)) continue;
+            keys.add(key);
+            queue.push(rooms[key]);
+        }
+    }
+    return keys.size === rooms.length
+};
