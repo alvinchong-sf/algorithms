bd919e77521de99e4bbbeaf475db0cf16abd6e75
alvinchong.sf <lvinz0790@gmail.com>
2024-02-08 15:35:40 -0800

    is beautiful sting

---
+++ code_signal/arcade_intro/is_beautiful_string.py
@@ -0,0 +1,44 @@
+# A string is said to be beautiful if each letter in the string appears at most as 
+# many times as the previous letter in the alphabet within the string; ie: b 
+# occurs no more times than a; c occurs no more times than b; etc. Note that 
+# letter a has no previous letter.
+
+# Given a string, check whether it is beautiful.
+
+# Example
+
+# For inputString = "bbbaacdafe", the output should be solution(inputString) = true.
+
+# This string contains 3 as, 3 bs, 1 c, 1 d, 1 e, and 1 f (and 0 of every other letter), 
+# so since there aren't any letters that appear more frequently than the previous 
+# letter, this string qualifies as beautiful.
+
+# For inputString = "aabbb", the output should be solution(inputString) = false.
+
+# Since there are more bs than as, this string is not beautiful.
+
+# For inputString = "bbc", the output should be solution(inputString) = false.
+
+# Although there are more bs than cs, this string is not beautiful because there are 
+# no as, so therefore there are more bs than as.
+
+import string
+
+def solution(inputString):
+    alphabet = string.ascii_lowercase
+    arr = [0 for i in range(len(alphabet))]
+    
+    for c in inputString:
+        idx = alphabet.index(c)
+        arr[idx] += 1
+    
+    last_num = arr[0]
+    
+    for i in range(0, len(arr), 2):
+        curr = arr[i]
+        next = arr[i+1]
+        if next > curr: return False
+        if next > last_num: return False
+        last_num = curr
+    
+    return True
