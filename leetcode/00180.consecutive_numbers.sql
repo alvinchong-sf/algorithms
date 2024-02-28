5f7e221b9e9ed7cd0fbd1cad884b8abb87378cbe
alvinchong.sf <lvinz0790@gmail.com>
2024-02-27 19:54:32 -0800

    consecutive numbers

---
+++ leetcode/00180.consecutive_numbers.sql
@@ -0,0 +1,54 @@
+-- 180. Consecutive Numbers
+-- Table: Logs
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | num         | varchar |
+-- +-------------+---------+
+-- In SQL, id is the primary key for this table.
+-- id is an autoincrement column.
+ 
+
+-- Find all numbers that appear at least three times consecutively.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Logs table:
+-- +----+-----+
+-- | id | num |
+-- +----+-----+
+-- | 1  | 1   |
+-- | 2  | 1   |
+-- | 3  | 1   |
+-- | 4  | 2   |
+-- | 5  | 1   |
+-- | 6  | 2   |
+-- | 7  | 2   |
+-- +----+-----+
+-- Output: 
+-- +-----------------+
+-- | ConsecutiveNums |
+-- +-----------------+
+-- | 1               |
+-- +-----------------+
+-- Explanation: 1 is the only number that appears consecutively for at least three times.
+
+select distinct L2.num as ConsecutiveNums
+from Logs as L1
+cross join Logs as L2
+where L1.num = L2.num
+and (L2.id - L1.id) <= 2 
+and (l2.id - l1.id) >= 0
+group by L1.id
+having count(L1.id) = 3
+order by L1.num, L1.id
+;
