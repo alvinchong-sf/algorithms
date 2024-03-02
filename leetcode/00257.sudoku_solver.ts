62c39327837b0bdc0c123673c3dd00779f1b5632
alvinchong.sf <lvinz0790@gmail.com>
2024-03-01 18:34:12 -0800

    sudoku solver

--- leetcode/00257.sudoku_solver.ts
+++ leetcode/00257.sudoku_solver.ts
@@ -1,7 +1,7 @@
-// https://leetcode.com/problems/sudoku-solver/description/
 // 37. Sudoku Solver
-// Write a program to solve a Sudoku puzzle by filling the empty cells.
+// https://leetcode.com/problems/sudoku-solver/description/
 
+// Write a program to solve a Sudoku puzzle by filling the empty cells.
 // A sudoku solution must satisfy all of the following rules:
 
 // Each of the digits 1-9 must occur exactly once in each row.
@@ -9,11 +9,7 @@
 // Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
 // The '.' character indicates empty cells.
 
- 
-
 // Example 1:
-
-
 // Input: board = [
 //     ["5","3",".",".","7",".",".",".","."],
 //     ["6",".",".","1","9","5",".",".","."],
