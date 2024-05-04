56a94c69c5c3d34c8a04af6e942f3743bd88173a
alvinchong.sf <lvinz0790@gmail.com>
2024-05-04 01:19:56 -0700

    Frog Jump

---
+++ leetcode/00301.frog_jump.py
@@ -0,0 +1,49 @@
+# 403. Frog Jump
+# A frog is crossing a river. The river is divided into some number of units, and at each unit,
+# there may or may not exist a stone. The frog can jump on a stone, but it must not jump into 
+# the water. Given a list of stones positions (in units) in sorted ascending order, determine 
+# if the frog can cross the river by landing on the last stone. Initially, the frog is on the 
+# first stone and assumes the first jump must be 1 unit. If the frog's last jump was k units, 
+# its next jump must be either k - 1, k, or k + 1 units. The frog can only jump in the forward direction.
+
+# Example 1:
+# Input: stones = [0,1,3,5,6,8,12,17]
+# Output: true
+# Explanation: The frog can jump to the last stone by jumping 1 unit to the 2nd stone, then 
+# 2 units to the 3rd stone, then 2 units to the 4th stone, then 3 units to the 6th stone, 4 
+# units to the 7th stone, and 5 units to the 8th stone.
+
+# Example 2:
+# Input: stones = [0,1,2,3,4,8,9,11]
+# Output: false
+# Explanation: There is no way to jump to the last stone as the gap between the 5th and 6th 
+# stone is too large.
+ 
+# Constraints:
+# 2 <= stones.length <= 2000
+# 0 <= stones[i] <= 231 - 1
+# stones[0] == 0
+# stones is sorted in a strictly increasing order.
+
+# https://leetcode.com/problems/frog-jump/description/
+# Time: O(n^2) | Space: O(n)
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        n = len(stones)
+        memo = {}
+        return self.dfs(0, stones, n, 0, memo)
+
+    def dfs(self, i, stones, n, k, memo) -> bool:
+        if i >= n - 1: return True
+        if (i, k) in memo: return memo[(i, k)]
+        
+        for j in range(i + 1, n):
+            diff = stones[j] - stones[i]
+            if diff == k + 1 or diff == k - 1 or diff == k:
+                can_cross_river = self.dfs(j, stones, n, diff, memo)
+                if can_cross_river:
+                    memo[(i, k)] = True
+                    return True
+
+        memo[(i, k)] = False
+        return False
