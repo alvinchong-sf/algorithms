ae4ebeca29123b031340cd9317a9b29350af4d58
alvinchong.sf <lvinz0790@gmail.com>
2024-05-14 23:12:10 -0700

    Insert delete and getRandom O(1)

---
+++ leetcode/00312.insert_delete_getRandom_01.py
@@ -0,0 +1,58 @@
+# 380. Insert Delete GetRandom O(1)
+# Implement the RandomizedSet class:
+# RandomizedSet() Initializes the RandomizedSet object.
+# bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+# bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+# int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called).
+# Each element must have the same probability of being returned.
+# You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+# Example 1:
+# Input
+# ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+# [[], [1], [2], [2], [], [1], [2], []]
+# Output
+# [null, true, false, true, 2, true, false, 2]
+
+# Explanation
+# RandomizedSet randomizedSet = new RandomizedSet();
+# randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+# randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+# randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+# randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+# randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+# randomizedSet.insert(2); // 2 was already in the set, so return false.
+# randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+
+# https://leetcode.com/problems/insert-delete-getrandom-o1
+# Time: O(1) | Space: O(n)
+import random
+class RandomizedSet:
+    def __init__(self):
+        self.map = {}
+        self.arr = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.map:
+            return False
+        self.arr.append(val)
+        self.map[val] = len(self.arr) - 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.map:
+            return False
+        swap_idx = self.map[val]
+        swap_num = self.arr[-1]
+        self.swap(swap_idx, len(self.arr) - 1, self.arr)
+        self.arr.pop()
+        del self.map[val]
+        if swap_idx != len(self.arr):
+            self.map[swap_num] = swap_idx
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
+
+    def swap(self, i, j, nums):
+        nums[i], nums[j] = nums[j], nums[i]
