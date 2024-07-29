2a785f4d5d9d991f772c6b6d8042625302b33d98
alvinchong.sf <lvinz0790@gmail.com>
2024-07-29 14:18:47 -0700

    count number of teams

---
+++ leetcode/00388.count_number_of_teams.py
@@ -0,0 +1,51 @@
+"""
+1395. Count Number of Teams
+There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+You have to form a team of 3 soldiers amongst them under the following rules:
+    Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+    A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) 
+    where (0 <= i < j < k < n).
+Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
+
+Example 1:
+Input: rating = [2,5,3,4,1]
+Output: 3
+Explanation: We can form three teams given the conditions. (2,3,4), (5,4,1), (5,3,1). 
+
+Example 2:
+Input: rating = [2,1,3]
+Output: 0
+Explanation: We can't form any team given the conditions.
+
+Example 3:
+Input: rating = [1,2,3,4]
+Output: 4
+
+Constraints:
+    n == rating.length
+    3 <= n <= 1000
+    1 <= rating[i] <= 105
+    All the integers in rating are unique.
+
+https://leetcode.com/problems/count-number-of-teams/description/
+Time: O(n^2) | Space: O(n)
+"""
+
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        n = len(rating)
+        dp = [[0, 0] for _ in range(n)]
+        count = 0
+        for i in range(1, n - 1):
+            for j in range(i + 1, n):
+                num1, num2 = rating[i], rating[j]
+                if num1 < num2:
+                    dp[i][0] += 1
+                else:
+                    dp[i][1] += 1
+
+        for idx1, r in enumerate(rating):
+            for idx2 in range(idx1 + 1, n):
+                count1, count2 = dp[idx2]
+                count = count + count1 if r < rating[idx2] else count + count2
+        return count
