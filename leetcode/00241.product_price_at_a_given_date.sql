ae3690152a2001595d78dcb512ca9607819ab31e
alvinchong.sf <lvinz0790@gmail.com>
2024-08-04 01:13:42 -0700

    refactor: product price at a given date

--- leetcode/00241.product_price_at_a_given_date.sql
+++ leetcode/00241.product_price_at_a_given_date.sql
@@ -9,19 +9,12 @@
 -- +---------------+---------+
 -- (product_id, change_date) is the primary key (combination of columns with unique values) of this table.
 -- Each row of this table indicates that the price of some product was changed to a new price at some date.
- 
-
 -- Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products 
 -- before any change is 10.
-
 -- Return the result table in any order.
-
 -- The result format is in the following example.
 
- 
-
 -- Example 1:
-
 -- Input: 
 -- Products table:
 -- +------------+-----------+-------------+
@@ -34,6 +27,7 @@
 -- | 2          | 65        | 2019-08-17  |
 -- | 3          | 20        | 2019-08-18  |
 -- +------------+-----------+-------------+
+
 -- Output: 
 -- +------------+-------+
 -- | product_id | price |
@@ -43,25 +37,22 @@
 -- | 3          | 10    |
 -- +------------+-------+
 
-(
-    select P1.product_id as product_id, P1.new_price as price
-    from Products as P1
-    where P1.change_date <= '2019-08-16' 
-    and change_date = (
-        select max(change_date) 
-        from Products as P2 
-        where P2.product_id = P1.product_id 
-        and P2.change_date <= '2019-08-16' 
-    )
+with T1 as (
+    select product_id, max(change_date) as change_date
+    from Products
+    where change_date <= "2019-08-16"
+    group by product_id
+), T2 as (
+    select distinct product_id
+    from Products
+    where change_date > "2019-08-16"
+), T3 as (
+    select product_id, 10 as price
+    from T2
+    where product_id not in (select product_id from T1)
+), T4 as (
+    select P.product_id, P.new_price as price
+    from Products as P
+    where (P.product_id, P.change_date) in (select T1.product_id, T1.change_date from T1)
 )
-union all
-(
-    select distinct product_id, 10 as new_price
-    from Products as P3
-    where product_id not in (
-        select P4.product_id as product_id
-        from Products as P4
-        where P3.product_id = P4.product_id
-        and change_date <= '2019-08-16'
-    )
-)
+(select * from T4) union all (select * from T3)
