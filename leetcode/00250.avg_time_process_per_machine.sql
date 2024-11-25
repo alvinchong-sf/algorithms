1e78877f962793bd90d844db902cafd652891565
alvinchong.sf <lvinz0790@gmail.com>
2024-11-24 16:57:54 -0800

    postgresql solution using cast

--- leetcode/00250.avg_time_process_per_machine.sql
+++ leetcode/00250.avg_time_process_per_machine.sql
@@ -77,3 +77,12 @@ and A1.activity_type != A2.activity_type
 and A1.timestamp <= A2.timestamp
 group by machine_id
 ;
+
+-- postgresql using cast
+select A1.machine_id,
+round(cast(avg(A2.timestamp - A1.timestamp) as NUMERIC), 3) as processing_time
+from Activity as A1, Activity as A2
+where A1.machine_id = A2.machine_id and
+A1.process_id = A2.process_id and 
+A1.activity_type = 'start' and A2.activity_type = 'end'
+group by A1.machine_id
